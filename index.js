8b5b30a4c6c58b625ce37c3a67e4052a5bdb10f8
gauravsahu9668 <sahug6194@gmail.com>
2024-12-21 17:00:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express=require("express")
+const app=express()
+const mailsender=require('./mailsender')
+require("dotenv").config()
+app.use(express.json())
+const cors=require('cors')
+const PORT=process.env.PORT || 4000
+app.use(cors({
+    origin:"http://localhost:3000",
+    credentials:true,
+}))
+app.post('/send-email',async(req,res)=>{
+    try{
+        const {email,title,html}=req.body;
+        await mailsender(email,title,html)
+        return res.json({
+            success:true
+        })
+    }catch(e){
+        return res.json({
+            success:true,
+            mess:"nhi hua"
+        })
+    }
+})
+app.get('/',async(req,res)=>{
+    return res.json({
+        success:false,
+        message:"run ho gya hai"
+    })
+})
+app.listen(PORT,()=>{
+    console.log(`app is running at port ${PORT}`)
+})
